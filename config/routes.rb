e5b37fe41a08ac7bf3d33e449af17fd7a3d39a23
kjlilchild <kodylittlechild@gmail.com>
2022-01-13 21:17:18 -0800

    added CSS w/ Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "lists#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
