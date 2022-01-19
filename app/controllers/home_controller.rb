1bab4c20dc123e1308c7399d895669ded5d1737a
kjlilchild <kodylittlechild@gmail.com>
2022-01-18 19:35:39 -0800

    style updates. Cleaned up CSS.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,10 @@ class HomeController < ApplicationController
 
 	end
 
+	def trash_em_all
+		@trash_em_all = List.all.destroy_all
+		redirect_to lists_path, notice: "All Marked Items Destroyed"
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "lists#index"
   get "home/trash_em"
+  get "home/trash_em_all"
   # Defines the root path route ("/")
   # root "articles#index"
 end
